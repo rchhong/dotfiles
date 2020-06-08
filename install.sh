35a06fc83359b2ae322f7ae90d2cfb8a029c724f
Ryan Chhong <ryanchhong@gmail.com>
2020-06-08 13:34:38 -0500

    Updated Install to support Mac OS

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 DOTFILES = $HOME/.dotfiles
-# Install git
-sudo apt install git
-git clone https://github.com/InfernalHydra/dotfiles.git $DOTFILES
+
 
 if [ "$(uname)" == "Darwin" ]; then
     echo "Configuring MacOS"
+    xcode-select --install
+    git clone https://github.com/InfernalHydra/dotfiles.git $DOTFILES
     $DOTFILES/scripts/installOSX.sh
 elif [ "$(uname)" == "Linux" ]; then
     echo "Configuring Linux"
+    # Install git
+    sudo apt install git
+    git clone https://github.com/InfernalHydra/dotfiles.git $DOTFILES
     $DOTFILES/scripts/installLinux.sh
 fi
