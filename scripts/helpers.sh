dc8494bd331f84553706df601e1f4d73f3270341
Ryan Chhong <ryanchhong@gmail.com>
2023-05-08 19:52:09 -0500

    remove unnecessary helper functions

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -23,29 +23,6 @@ function print_error {
 	printf "${RED}${1}${NC}\n"
 }
 
-# https://github.com/fearside/ProgressBar/blob/master/progressbar.sh
-# Author : Teddy Skarin
-
-PROGRESS_BAR_NUM_DONE=0
-PROGRESS_BAR_TOTAL=0
-
-function setup_progress_bar {
-	PROGRES_BAR_NUM_DONE=0
-	PROGRESS_BAR_TOTAL=${1}
-}
-
-function progress_bar {
-	let _progress=(${PROGRESS_BAR_NUM_DONE}*100/${PROGRESS_BAR_TOTAL}*100)/100
-	let _done=(${_progress}*4)/10
-	let _left=40-$_done
-	_done=$(printf "%${_done}s")
-	_left=$(printf "%${_left}s")
-	print_info "\r${1}\n Progress : [${_done// /#}${_left// /-}] ${_progress}%%"
-	((PROGRESS_BAR_NUM_DONE++))
-}
-
-export -f setup_progress_bar
-export -f progress_bar
 export -f print_stage
 export -f print_info
 export -f print_success
