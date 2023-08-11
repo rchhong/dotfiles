d3f8e3ce566abec805f1418ada4aee637b10a6e4
Ryan Chhong <ryanchhong@gmail.com>
2023-08-11 17:07:30 -0400

    settings tweaks

---
+++ ubuntu/install_settings.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DOTFILES=$HOME/.dotfiles
+TEMP=$DOTFILES/temp/
+source $DOTFILES/scripts/helpers.sh
+
+# Settings
+# For more modifications, run `dconf watch /` and see what changes
+
+# Disable Hot Corners
+gsettings set org.gnome.desktop.interface enable-hot-corners false
+
+# Change order of buttons in Window
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
+
+# Show hidden files
+gsettings set org.gnome.nautilus.preferences show-hidden-files true
+
+# Move dock to bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+
+# Show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# Show day of the week 
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+
+# Dark theme
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-dark'
+
+# Enable some built-in extensions, don't want to load more cause work stuff
+gsettings set org.gnome.shell enabled-extensions ['ubuntu-appindicators@ubuntu.com', 'ubuntu-dock@ubuntu.com']
+
+# Autohide dock
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+
+# Optional: Install this to move the clock to the left https://extensions.gnome.org/extension/2/move-clock/
+
+# Setup dock
+gsettings set org.gnome.shell favorite-apps ['org.gnome.Nautilus.desktop', 'firefox.desktop', 'kitty.desktop', 'code.desktop', 'postman.desktop', 'obsidian.desktop']
+
+
+print_success "Done!"
