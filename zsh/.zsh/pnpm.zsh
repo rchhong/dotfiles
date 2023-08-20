9fba4024228c40798f4c8208ffb774be6afdb3cc
Ryan Chhong <ryanchhong@gmail.com>
2023-08-20 18:09:08 -0500

    fix issue with pnpm

--- zsh/.zsh/pnpm.zsh
+++ zsh/.zsh/pnpm.zsh
@@ -1,5 +1,5 @@
 # pnpm
-export PNPM_HOME="/home/ryanc/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
