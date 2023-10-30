a225329287b626375df93c0691e25ad954dccfc4
Ryan Chhong <ryanchhong@gmail.com>
2023-10-29 20:42:01 -0500

    nvim: added shortcuts for commands, redo gitsigns shortcuts

--- nvim/.config/nvim/lua/plugins/markdown_preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown_preview.lua
@@ -2,6 +2,9 @@
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    keys = {
+        {"<Leader>mdp", "<cmd>MarkdownPreviewToggle<CR>", mode={"n"}, desc="Markdown Preview Toggle"}
+    },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
 }
--- nvim/.config/nvim/lua/plugins/neogen.lua
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -2,6 +2,9 @@ return {
     "danymat/neogen",
     dependencies = "nvim-treesitter/nvim-treesitter",
     cmd = { "Neogen" },
+    keys = {
+        {"<Leader>ng", function() require("neogen").generate() end, mode={"n"}, desc="Neogen"}
+    },
     opts = {
         snippet_engine = "luasnip"
     },
