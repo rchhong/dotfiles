507875260c741d2c1e67ba63e6dc60ed4f711b92
Ryan Chhong <ryanchhong@gmail.com>
2023-10-30 21:33:03 -0500

    nvim: add move line mappings, changed mapping in kitty to support this

--- nvim/.config/nvim/lua/config/misc.lua
+++ nvim/.config/nvim/lua/config/misc.lua
@@ -30,4 +30,23 @@ vim.keymap.set('n', '<Down>', "(v:count == 0 ? 'gj' : 'j')", { noremap = true, s
 vim.keymap.set('n', 'k', "(v:count == 0 ? 'gk' : 'k')", { noremap = true, silent = true, expr = true })
 vim.keymap.set('n', '<Up>', "(v:count == 0 ? 'gk' : 'k')", { noremap = true, silent = true, expr = true })
 
+-- Better indenting
+m.vmap("<", "<gv")
+m.vmap(">", ">gv")
+
+-- Move lines around
+m.map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+m.map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+m.map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+m.map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+m.map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+m.map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+
+-- Move lines around
+m.map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+m.map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+m.map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+m.map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+m.map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+m.map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
