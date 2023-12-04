930c23835e38f764007711b4893597dcf7b24641
Ryan Chhong <ryanchhong@gmail.com>
2023-12-04 08:46:28 -0600

    nvim: config updates

--- nvim/.config/nvim/lua/plugins/code/jupyter/jupynium.lua
+++ nvim/.config/nvim/lua/plugins/code/jupyter/jupynium.lua
@@ -1,11 +1,12 @@
 return {
     "kiyoon/jupynium.nvim",
-    build = "micromamba install -n base --yes nb_conda_kernels 2> /dev/null && micromamba run -a \"\" -n base pip install ~/.local/share/nvim/lazy/jupytext.nvim/.",
+    build = "micromamba install -n base --yes nb_conda_kernels 2> /dev/null && micromamba run -a \"\" -n base pip install notebook nbclassic jupyter-console && micromamba run -a \"\" -n base pip install ~/.local/share/nvim/lazy/jupytext.nvim/pyproject.toml",
     enabled = vim.fn.isdirectory(vim.fn.expand "~/micromamba/envs/base"),
     dependencies = {
       "rcarriga/nvim-notify",   -- optional
       "stevearc/dressing.nvim", -- optional, UI for :JupyniumKernelSelect
     },
+    ft = {"python"},
     opts = {
         python_host = { "micromamba", "run", "-a", "\"\"", "-n", "base", "python" },
         jupyter_command = { "micromamba", "run", "-a", "\"\"", "-n", "base", "jupyter" },
@@ -15,11 +16,11 @@ return {
         },
         auto_attach_to_server = {
             enable = true,
-            file_pattern = { "*.ju.*", "*.ipynb", "*.md" },
+            file_pattern = { "*.ju.*", "*.ipynb" },
         },
-        auto_sync_server = {
+        auto_start_sync = {
             enable = true,
-            file_pattern = { "*.ju.*", "*.ipynb", "*.md"},
+            file_pattern = { "*.ju.*", "*.ipynb" },
         },
         jupynium_file_pattern = { "*.ju.*", "*.ipynb" },
     }
