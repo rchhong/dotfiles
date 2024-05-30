30046b159ccf6aedc035170922fb7e39b6abf379
Ryan Chhong <ryanchhong@gmail.com>
2024-05-30 16:27:01 -0500

    nvim: trouble changes

--- nvim/.config/nvim/lua/plugins/tools/telescope.lua
+++ nvim/.config/nvim/lua/plugins/tools/telescope.lua
@@ -105,13 +105,13 @@ return {
         require('telescope').load_extension('fzf')
 
         local actions = require("telescope.actions")
-        local trouble = require("trouble.providers.telescope")
+        local trouble = require("trouble.sources.telescope")
 
         local telescope = require("telescope")
 
         opts.defaults.mappings = {
-            i = { ["<c-t>"] = trouble.open_with_trouble },
-            n = { ["<c-t>"] = trouble.open_with_trouble },
+            i = { ["<c-t>"] = trouble.open },
+            n = { ["<c-t>"] = trouble.open },
         }
 
         require('telescope').setup(opts)
--- nvim/.config/nvim/lua/plugins/ui/trouble.lua
+++ nvim/.config/nvim/lua/plugins/ui/trouble.lua
@@ -1,21 +1,85 @@
 return {
-    "folke/trouble.nvim",
-    cmd = {
-        "TroubleToggle",
-    },
-    dependencies = {
-        'nvim-tree/nvim-web-devicons'
-    },
-    keys = {
-        {"<leader>xx", function() require("trouble").toggle() end, desc = "Trouble: toggle", mode = {'n'}},
-        {"<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end, desc = "Trouble: workspace diagnostics", mode = {'n'}},
-        {"<leader>xd", function() require("trouble").toggle("document_diagnostics") end, desc = "Trouble: document diagnostics", mode = {'n'}},
-        {"<leader>xl", function() require("trouble").toggle("quickfix") end, desc = "Trouble: quickfix menu", mode = {'n'}},
-        {"<leader>xq", function() require("trouble").toggle("loclist") end, desc = "Trouble: location list", mode = {'n'}},
-        {"<leader>xr", function() require("trouble").toggle("lsp_references") end, desc = "Trouble: LSP references", mode = {'n'}},
-    },
-    opts = {
-        auto_open = false,
-        auto_close = false,
-    },
+	"folke/trouble.nvim",
+	cmd = {
+		"TroubleToggle",
+	},
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{
+			"<leader>xx",
+			function()
+				require("trouble").toggle({ mode = "diagnostics" })
+			end,
+			desc = "Trouble: toggle",
+			mode = { "n" },
+		},
+		{
+			"<leader>xw",
+			function()
+				require("trouble").toggle({
+					mode = "diagnostics", -- inherit from diagnostics mode
+					filter = {
+						any = {
+							buf = 0, -- current buffer
+							{
+								-- limit to files in the current project
+								function(item)
+									return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+								end,
+							},
+						},
+					},
+				})
+			end,
+			desc = "Trouble: workspace diagnostics",
+			mode = { "n" },
+		},
+		{
+			"<leader>xd",
+			function()
+				require("trouble").toggle({
+					mode = "diagnostics", -- inherit from diagnostics mode
+					filter = {
+						any = {
+							buf = 0, -- current buffer
+						},
+					},
+				})
+			end,
+			desc = "Trouble: document diagnostics",
+			mode = { "n" },
+		},
+		{
+			"<leader>xq",
+			function()
+				require("trouble").toggle({
+					mode = "qflist",
+				})
+			end,
+			desc = "Trouble: quickfix menu",
+			mode = { "n" },
+		},
+		{
+			"<leader>xL",
+			function()
+				require("trouble").toggle({ mode = "loclist" })
+			end,
+			desc = "Trouble: location list",
+			mode = { "n" },
+		},
+		{
+			"<leader>xl",
+			function()
+				require("trouble").toggle({ mode = "lsp" })
+			end,
+			desc = "Trouble: LSP references",
+			mode = { "n" },
+		},
+	},
+	opts = {
+		auto_open = false,
+		auto_close = false,
+	},
 }
