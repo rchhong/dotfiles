1828a81ceed5b6ca94d1c1216889ac24b4859e1a
Ryan Chhong <ryanchhong@gmail.com>
2024-06-14 09:16:39 -0500

    nvim: format lua files

--- nvim/.config/nvim/lua/plugins/code/jupyter/molten.lua
+++ nvim/.config/nvim/lua/plugins/code/jupyter/molten.lua
@@ -1,32 +1,50 @@
 return {
-    -- Requires external dependencies
-    -- pip install pynvim jupyter_client cairosvg pnglatex plotly kaleido pyperclip nbformat
-    "benlubas/molten-nvim",
-    build = {
-        "micromamba run -a \"\" -n base pip install notebook nbclassic jupyter-console cairosvg pnglatex plotly kaleido pyperclip",
-        ":UpdateRemotePlugins"
-    },
-    dependencies = {
-        "3rd/image.nvim",
-    },
-    ft = {
-        "markdown",
-        "python",
-        "quarto"
-    },
-    keys = {
-        {"<leader>me", "<cmd>MoltenEvaluateOperator<CR>", desc="Molten: evaluate operator", mode={"n"}, silent=true},
-        {"<leader>mos", "<cmd>noautocmd MoltenEnterOutput<CR>", desc="Molten: open output window", mode={"n"}, silent=true},
-        {"<leader>mrr", "<cmd>MoltenReevaluateCell<CR>", desc="Molten: re-evaluate cell", mode={"n"}, silent=true},
-        {"<leader>mr", "<cmd><C-u>MoltenEvaluateVisual<CR>gv", desc="Molten: execute visual selection", mode={"v"}, silent=true},
-        {"<leader>moh", "<cmd>MoltenHideOutput<CR>", desc="Molten: close output window", mode={"n"}, silent=true},
-        {"<leader>mmd", "<cmd>MoltenDelete<CR>", desc="Molten: delete Molten cell", mode={"n"}, silent=true},
-        {"<leader>mmx", "<cmd>MoltenOpenInBrowser<CR>", desc="Molten: open output in browser", mode={"n"}, silent=true},
-        {"<leader>mt", "<cmd>MoltenInterrupt<CR>", desc="Molten: terminate kernel", mode={"n"}, silent=true},
-    },
-    init = function()
-        -- Need to set a keybind for `:noautocmd MoltenEnterOutput` to open the output again
-        vim.g.molten_auto_open_output = false
+	-- Requires external dependencies
+	-- pip install pynvim jupyter_client cairosvg pnglatex plotly kaleido pyperclip nbformat
+	"benlubas/molten-nvim",
+	build = {
+		'micromamba run -a "" -n base pip install notebook nbclassic jupyter-console cairosvg pnglatex plotly kaleido pyperclip',
+		":UpdateRemotePlugins",
+	},
+	dependencies = {
+		"3rd/image.nvim",
+	},
+	ft = {
+		"markdown",
+		"python",
+		"quarto",
+	},
+	keys = {
+		{ "<leader>me", "<cmd>MoltenEvaluateOperator<CR>", desc = "Molten: evaluate operator", mode = { "n" }, silent = true },
+		{
+			"<leader>mos",
+			"<cmd>noautocmd MoltenEnterOutput<CR>",
+			desc = "Molten: open output window",
+			mode = { "n" },
+			silent = true,
+		},
+		{ "<leader>mrr", "<cmd>MoltenReevaluateCell<CR>", desc = "Molten: re-evaluate cell", mode = { "n" }, silent = true },
+		{
+			"<leader>mr",
+			"<cmd><C-u>MoltenEvaluateVisual<CR>gv",
+			desc = "Molten: execute visual selection",
+			mode = { "v" },
+			silent = true,
+		},
+		{ "<leader>moh", "<cmd>MoltenHideOutput<CR>", desc = "Molten: close output window", mode = { "n" }, silent = true },
+		{ "<leader>mmd", "<cmd>MoltenDelete<CR>", desc = "Molten: delete Molten cell", mode = { "n" }, silent = true },
+		{
+			"<leader>mmx",
+			"<cmd>MoltenOpenInBrowser<CR>",
+			desc = "Molten: open output in browser",
+			mode = { "n" },
+			silent = true,
+		},
+		{ "<leader>mt", "<cmd>MoltenInterrupt<CR>", desc = "Molten: terminate kernel", mode = { "n" }, silent = true },
+	},
+	init = function()
+		-- Need to set a keybind for `:noautocmd MoltenEnterOutput` to open the output again
+		vim.g.molten_auto_open_output = false
 
 		-- Don't forget to setup and install the plugin if you want to view image outputs
 		vim.g.molten_image_provider = "image.nvim"
