ea6e20da76fb2b29440079d16343d78fc3267409
Ryan Chhong <ryanchhong@gmail.com>
2025-02-21 11:32:43 -0600

    nvim: added Lazy.nvim bindings to lazydev

--- nvim/.config/nvim/lua/plugins/code/lua/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/code/lua/lazydev.lua
@@ -7,6 +7,9 @@ return {
 				-- See the configuration section for more details
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+				{ path = "LazyVim", words = { "LazyVim" } },
+				{ path = "snacks.nvim", words = { "Snacks" } },
+				{ path = "lazy.nvim", words = { "LazyVim" } },
 			},
 		},
 	},
