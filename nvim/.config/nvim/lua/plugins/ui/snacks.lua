cba3e4f60c773d6d8760b52b57de4942fb250e29
Ryan Chhong <ryanchhong@gmail.com>
2025-02-23 20:46:48 -0600

    nvim: added more toggles to Snacks.nvim

--- nvim/.config/nvim/lua/plugins/ui/snacks.lua
+++ nvim/.config/nvim/lua/plugins/ui/snacks.lua
@@ -112,7 +112,6 @@ return {
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     -- git
     { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
-    { "<leader>gbl", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
     { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
     { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
     { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
@@ -209,14 +208,23 @@ return {
 					.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
 					:map("<leader>uc")
 				Snacks.toggle.inlay_hints():map("<leader>uh")
+				Snacks.toggle({
+					name = "Git Signs",
+					get = function()
+						return require("gitsigns.config").config.signcolumn
+					end,
+					set = function(state)
+						require("gitsigns").toggle_signs(state)
+					end,
+				}):map("<leader>uG")
+				Snacks.toggle.treesitter():map("<leader>uT")
+				Snacks.toggle.indent():map("<leader>ug")
 				-- Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
 				-- Snacks.toggle.line_number():map("<leader>ul")
-				-- Snacks.toggle.treesitter():map("<leader>uT")
 				-- Snacks.toggle
 				-- 	.option("background", { off = "light", on = "dark", name = "Dark Background" })
 				-- 	:map("<leader>ub")
 				--
-				-- Snacks.toggle.indent():map("<leader>ug")
 				-- Snacks.toggle.dim():map("<leader>uD")
 			end,
 		})
