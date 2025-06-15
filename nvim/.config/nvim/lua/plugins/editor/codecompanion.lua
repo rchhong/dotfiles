28d2ad09ecb9169c5e9222dc8c10d318c8560e22
Ryan Chhong <ryanchhong@gmail.com>
2025-06-15 14:37:19 -0500

    nvim: updates to codecompanion.nvim

--- nvim/.config/nvim/lua/plugins/editor/codecompanion.lua
+++ nvim/.config/nvim/lua/plugins/editor/codecompanion.lua
@@ -31,9 +31,45 @@ return {
 			noremap = true,
 		},
 	},
-	opts = {
-		log_level = "ERROR",
-		language = "English",
-	},
+	opts = function()
+		local adapters = require("codecompanion.adapters")
+		return {
+			log_level = "ERROR",
+			language = "English",
+			adapters = {
+				anthropic = function()
+					return adapters.extend("anthropic", {
+						env = {
+							api_key = "cmd:cat ~/.dotfiles/private/anthropic_key.txt | awk '{$1=$1;print}'",
+						},
+					})
+				end,
+				-- llama3 = function()
+				-- 	return require("codecompanion.adapters").extend("ollama", {
+				-- 		name = "llama3", -- Give this adapter a different name to differentiate it from the default ollama adapter
+				-- 		schema = {
+				-- 			model = {
+				-- 				default = "llama3:latest",
+				-- 			},
+				-- 			num_ctx = {
+				-- 				default = 16384,
+				-- 			},
+				-- 			num_predict = {
+				-- 				default = -1,
+				-- 			},
+				-- 		},
+				-- 	})
+				-- end,
+			},
+			strategies = {
+				chat = {
+					adapter = "anthropic",
+				},
+				inline = {
+					adapter = "anthropic",
+				},
+			},
+		}
+	end,
 	config = true,
 }
