62d5a336b6d54af64db346c7931ca52763e13162
Ryan Chhong <ryanchhong@gmail.com>
2026-03-20 18:07:13 -0500

    nvim: add binding to open claude code

--- nvim/.config/nvim/lua/plugins/editor/sidekick.lua
+++ nvim/.config/nvim/lua/plugins/editor/sidekick.lua
@@ -71,5 +71,10 @@ return {
       function() require("sidekick.cli").toggle({ name = "opencode", focus = true }) end,
       desc = "Sidekick Toggle OpenCode",
     },
+    {
+      "<leader>ac",
+      function() require("sidekick.cli").toggle({ name = "claude", focus = true }) end,
+      desc = "Sidekick Toggle Claude",
+    }
   },
 }
