aae380cbd1ba4e802ef0a1de34151188c5b341e7
Ryan Chhong <ryanchhong@gmail.com>
2026-04-04 10:51:57 -0500

    nvim: lazydev integration with blink

--- nvim/.config/nvim/lua/plugins/editor/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/editor/blink-cmp.lua
@@ -40,6 +40,16 @@ return {
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
+			per_filetype = {
+				lua = { inherit_defaults = true, "lazydev" },
+			},
+			providers = {
+				lazydev = {
+				name = "LazyDev",
+				module = "lazydev.integrations.blink",
+				score_offset = 100, -- show at a higher priority than lsp
+				},
+			},
 		},
 		-- Use mini.snippets
 		snippets = { preset = "mini_snippets" },
