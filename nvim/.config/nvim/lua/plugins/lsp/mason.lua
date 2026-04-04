5a3d2c45b39c023e97551781644bbceeaf0c7478
Ryan Chhong <ryanchhong@gmail.com>
2026-04-04 12:37:11 -0500

    nvim: remove lazy dependency

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -22,9 +22,8 @@ return {
 		mr:on("package:install:success", function()
 		vim.defer_fn(function()
 			-- trigger FileType event to possibly load this newly installed LSP server
-			require("lazy.core.handler.event").trigger({
-			event = "FileType",
-			buf = vim.api.nvim_get_current_buf(),
+			vim.api.nvim_exec_autocmds("FileType", {
+				buffer = vim.api.nvim_get_current_buf(),
 			})
 		end, 100)
 		end)
